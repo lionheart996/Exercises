0491c1f515b82d64e23032b56cfd2a9d58bc1013
lionheart996 <zefir.kulev1@gmail.com>
2026-06-23 13:56:39 +0200

    Re-solved string explosion

--- Text Processing/string_explosion.py
+++ Text Processing/string_explosion.py
@@ -1,19 +1,55 @@
-string_explosion = input()
+# string_explosion = input()
+#
+# strength = 0
+# result = []
+#
+# for i in range(len(string_explosion)):
+#     ch = string_explosion[i]
+#
+#     if ch == ">":
+#         result.append(ch)
+#         strength += int(string_explosion[i + 1])
+#
+#     elif strength > 0:
+#         strength -= 1
+#
+#     else:
+#         result.append(ch)
+#
+# print("".join(result))
+from multiprocessing.reduction import steal_handle
+
+string = input()
 
 strength = 0
-result = []
+output = ''
 
-for i in range(len(string_explosion)):
-    ch = string_explosion[i]
-
-    if ch == ">":
-        result.append(ch)
-        strength += int(string_explosion[i + 1])
-
-    elif strength > 0:
+for index in range(len(string)):
+    if strength > 0 and string[index] != '>':
         strength -= 1
-
+    elif string[index] == '>':
+        output += string[index]
+        strength += int(string[index+1])
     else:
-        result.append(ch)
+        output += string[index]
 
-print("".join(result))
+print(output)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
